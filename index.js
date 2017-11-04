0556c99226ee883fd3900f4a809b11383133c3e4
Gavin Loughridge <gavinlough@gmail.com>
2017-11-04 14:57:19 -0500

    cleaned up code and added comments

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+/*
+  set variables
+*/
+//set initial selected color
 let selectedColor = 'black';
+
+//set available colors in palet
 let paletColors = ['black','white','green','blue','red','yellow'];
 
-for(let i = 0; i < 100; i++) {
+//set number of rows and columns in canvas
+let gridSize = 10;
+
+/*
+  create canvas
+*/
+
+//fill the grid with pixles
+for(let i = 0; i < gridSize * gridSize; i++) {
   canvas.appendChild(makePixle('white'));
 }
 
+//make and return a single pixle of a given start color
+function makePixle(color) {
+  let pixle = document.createElement('div');
+  pixle.style.width = 'calc((100% / ' + gridSize + ') - 2px)';
+  pixle.style.paddingBottom = 'calc((100% / ' + gridSize + ') - 2px)';
+  pixle.style.float = 'left';
+  pixle.style.backgroundColor = color;
+  pixle.style.border = '1px solid grey';
+  pixle.addEventListener('click', applyColor);
+  return pixle;
+}
+
+/*
+  create palet
+*/
+
+//fill the palet with color selectors
 for(let i = 0; i < paletColors.length; i++) {
   palet.appendChild(makePaletColor(paletColors[i]));
 }
 
+//make and return a single color selector
 function makePaletColor(color) {
   let paletColor = document.createElement('div');
   paletColor.style.width = 'calc(50% - 2px)';
@@ -20,22 +52,17 @@ function makePaletColor(color) {
   return paletColor;
 }
 
-function makePixle(color) {
-  let pixle = document.createElement('div');
-  pixle.style.width = 'calc(10% - 2px)';
-  pixle.style.paddingBottom = 'calc(10% - 2px)';
-  pixle.style.float = 'left';
-  pixle.style.backgroundColor = color;
-  pixle.style.border = '1px solid grey';
-  pixle.addEventListener('click', applyColor);
-  return pixle;
-}
+/*
+  event functions
+*/
 
+//apply the selected color to a pixle
 function applyColor() {
-  this.style.backgroundColor = selectedColor;
+  event.target.style.backgroundColor = selectedColor;
 }
 
+//set the selected color to a palet color
 function setColor() {
-  selectedColor = this.style.backgroundColor;
+  selectedColor = event.target.style.backgroundColor;
   indicator.style.backgroundColor = selectedColor;
 }
